9f18507b0642e647051c11bbcc10e1b2b6b07d93
hamzaezzzat <hamza2007ezzat@gmail.com>
2022-05-18 14:22:14 +0200

    Home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import React from "react";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 import "./App.css";
 export const App = () => {
     return (
         <>
             <Navbar />
+            <Home />
         </>
     );
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 function Home() {
-    return <div>Home</div>;
+    return (
+        <div className="hero">
+            <div class="card bg-dark text-white border-0">
+                <img
+                    src="./assets//bg.jpg"
+                    class="card-img"
+                    alt="background"
+                    height="550px"
+                />
+                <div class="card-img-overlay d-flex flex-column justify-content-center">
+                    <div className="container">
+                        <h5 class="card-title display-3 fw-bolder mb-0">
+                            NEW SEASON ARRIVALS
+                        </h5>
+                        <p class="card-text lead fs-2 ">
+                            CHECK OUT ALL THE TRENDS
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
