36a16da2c5ae3be5e7588208d6ff7249502277dd
hamzaezzzat <hamza2007ezzat@gmail.com>
2022-05-19 14:50:43 +0200

    commit

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -22,7 +22,47 @@ function Products() {
         };
         getProducts();
     }, []);
-    return <div>Products</div>;
+
+    const Loading = () => {
+        return <>Loading...</>;
+    };
+    const ShowProducts = () => {
+        return (
+            <div className="buttons">
+                <button className="btn btn-outline-dark">All</button>
+                <button className="btn btn-outline-dark me-2">
+                    Men's Clothing
+                </button>
+                <button className="btn btn-outline-dark me-2">
+                    Women's Clothing
+                </button>
+                <button className="btn btn-outline-dark me-2">
+                    Jewelery Clothing
+                </button>
+                <button className="btn btn-outline-dark me-2">
+                    Men's Clothing
+                </button>
+            </div>
+        );
+    };
+
+    return (
+        <div>
+            <div className="container my-5 py-5 ">
+                <div className="row">
+                    <div className="col-12 mb-5">
+                        <h1 className="display-6 fw-bolder text-center">
+                            Latest Products
+                        </h1>
+                        <hr />
+                    </div>
+                    <div className="row justify-content-center">
+                        {loading ? <Loading /> : <ShowProducts />}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Products;
