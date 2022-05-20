46be21fc70ee64a4436e9d2cbab9c4e25674f52b
hamzaezzzat <hamza2007ezzat@gmail.com>
2022-05-20 15:50:52 +0200

    add extra packages

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -28,21 +28,56 @@ function Products() {
     };
     const ShowProducts = () => {
         return (
-            <div className="buttons">
-                <button className="btn btn-outline-dark">All</button>
-                <button className="btn btn-outline-dark me-2">
-                    Men's Clothing
-                </button>
-                <button className="btn btn-outline-dark me-2">
-                    Women's Clothing
-                </button>
-                <button className="btn btn-outline-dark me-2">
-                    Jewelery Clothing
-                </button>
-                <button className="btn btn-outline-dark me-2">
-                    Men's Clothing
-                </button>
-            </div>
+            <>
+                <div className="buttons d-flex justify-content-center mb-5 pb-5">
+                    <button className="btn btn-outline-dark me-2">All</button>
+                    <button className="btn btn-outline-dark me-2">
+                        Men's Clothing
+                    </button>
+                    <button className="btn btn-outline-dark me-2">
+                        Women's Clothing
+                    </button>
+                    <button className="btn btn-outline-dark me-2">
+                        Jewelery Clothing
+                    </button>
+                    <button className="btn btn-outline-dark me-2">
+                        Electronics
+                    </button>
+                </div>
+                {filter.map((product) => {
+                    return (
+                        <>
+                            <div className="col-md-3 mb-4">
+                                <div
+                                    className="card h-100 text-center p-4"
+                                    key={product.id}
+                                >
+                                    <img
+                                        src={product.image}
+                                        className="card-img-top"
+                                        alt={product.title}
+                                        height="250px"
+                                    />
+                                    <div className="card-body">
+                                        <h5 className="card-title mb-0">
+                                            {product.title.substring(0, 12)}...
+                                        </h5>
+                                        <p className="card-text lead fw-bold">
+                                            ${product.price}
+                                        </p>
+                                        <a
+                                            href="#"
+                                            className="btn btn-outline-dark"
+                                        >
+                                            Buy Now
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </>
+                    );
+                })}
+            </>
         );
     };
 
